e9549c240f28ad371854110c7dbbf5b0ba7700e7
vhavlena <vohavlena@gmail.com>
2025-09-19 23:50:07 +0200

    postprocess: tunning the parameters

--- src/complement/complement_tela.cpp
+++ src/complement/complement_tela.cpp
@@ -57,8 +57,6 @@ spot::twa_graph_ptr kofola::apply_postprocessing(const spot::twa_graph_ptr& aut,
 		// for automata with many APs the reduction timeoutes
 		if(is_post_reduction_suitable(original_aut)) {
 			if(result->num_states() < 2000) {
-				p_post.set_level(spot::postprocessor::High);
-			} else {
 				p_post.set_level(spot::postprocessor::Low);
 			}
 			result = p_post.run(result);
