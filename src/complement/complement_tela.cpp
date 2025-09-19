1ce29c2f3f60740647501373eac52e3d4fee145d
vhavlena <vohavlena@gmail.com>
2025-09-19 23:37:17 +0200

    postprocessing: playing with params

--- src/complement/complement_tela.cpp
+++ src/complement/complement_tela.cpp
@@ -56,8 +56,8 @@ spot::twa_graph_ptr kofola::apply_postprocessing(const spot::twa_graph_ptr& aut,
 
 		// for automata with many APs the reduction timeoutes
 		if(is_post_reduction_suitable(original_aut)) {
-			if((kofola::OPTIONS.output_type == "buchi" || kofola::OPTIONS.output_type == "tgba")) {
-				if(result->num_states() < 2000) p_post.set_level(spot::postprocessor::Low);
+			if(result->num_states() < 2000) {
+				p_post.set_level(spot::postprocessor::High);
 			} else {
 				p_post.set_level(spot::postprocessor::Low);
 			}
