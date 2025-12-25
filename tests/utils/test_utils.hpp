eb5c98d39bd4ecaa761e433926d6cc9cc4e7eac3
vhavlena <vohavlena@gmail.com>
2025-12-25 15:39:04 +0100

    fix: remove problematic spot example

--- tests/utils/test_utils.hpp
+++ tests/utils/test_utils.hpp
@@ -92,7 +92,7 @@ const std::vector<std::string> COMMON_TEST_FILES = {
     "tests/test_data/tela_det/automaton900.hoa",
     "tests/test_data/tela_det/automaton940.hoa",
     "tests/test_data/tela_det/out_1332.hoa",
-    "tests/test_data/tela_det/out_3357.hoa",
+    //"tests/test_data/tela_det/out_3357.hoa",
 };
 
 /**
