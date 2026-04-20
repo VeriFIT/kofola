3a06bb8943906cec4c40fa10254976e3cde60b36
OndrejAlexaj <xalexa09@stud.fit.vutbr.cz>
2026-04-20 13:19:38 +0000

    update tests to reflect two shb modes

--- tests/tela/test_complement_tela.cpp
+++ tests/tela/test_complement_tela.cpp
@@ -52,12 +52,12 @@ TEST_CASE("complement_tela with tela_det_alg=inductive produces language-equival
     kofola::OPTIONS.params.erase("tela_det_alg");
 }
 
-TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_sh_break=yes produces language-equivalent results to Spot",
+TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_sh_break=root produces language-equivalent results to Spot",
           "[complement_tela][tela_det_alg][inductive][sd_ind_sh_break]") {
     // Set up TELA options and enable inductive SD-TELA determinization with shared breakpoint
     test_utils::setup_tela_options();
     kofola::OPTIONS.params["tela_det_alg"] = "inductive";
-    kofola::OPTIONS.params["sd_ind_sh_break"] = "yes";
+    kofola::OPTIONS.params["sd_ind_sh_break"] = "root";
 
     for (const std::string& filename : test_utils::COMMON_TEST_FILES) {
         SECTION("Testing file (inductive det, shared breakpoint): " + filename) {
@@ -74,12 +74,34 @@ TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_sh_break=yes p
     kofola::OPTIONS.params.erase("tela_det_alg");
 }
 
-TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_or_opt=yes produces language-equivalent results to Spot",
+TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_sh_break=inf_tree produces language-equivalent results to Spot",
+          "[complement_tela][tela_det_alg][inductive][sd_ind_sh_break]") {
+    // Set up TELA options and enable inductive SD-TELA determinization with shared breakpoint
+    test_utils::setup_tela_options();
+    kofola::OPTIONS.params["tela_det_alg"] = "inductive";
+    kofola::OPTIONS.params["sd_ind_sh_break"] = "inf_tree";
+
+    for (const std::string& filename : test_utils::COMMON_TEST_FILES) {
+        SECTION("Testing file (inductive det, shared breakpoint): " + filename) {
+            spot::twa_graph_ptr aut = test_utils::load_automaton_from_file(filename);
+            REQUIRE(aut != nullptr);
+
+            bool equivalent = test_utils::test_complement_equivalence(aut, false);
+            CHECK(equivalent);
+        }
+    }
+
+    // Clean up to avoid side-effects on other tests
+    kofola::OPTIONS.params.erase("sd_ind_sh_break");
+    kofola::OPTIONS.params.erase("tela_det_alg");
+}
+
+TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_or_opt=root produces language-equivalent results to Spot",
           "[complement_tela][tela_det_alg][inductive][sd_ind_or_opt]") {
     // Set up TELA options and enable inductive SD-TELA determinization with OR-FIN optimization
     test_utils::setup_tela_options();
     kofola::OPTIONS.params["tela_det_alg"] = "inductive";
-    kofola::OPTIONS.params["sd_ind_or_opt"] = "yes";
+    kofola::OPTIONS.params["sd_ind_or_opt"] = "root";
 
     for (const std::string& filename : test_utils::COMMON_TEST_FILES) {
         SECTION("Testing file (inductive det, OR-FIN opt): " + filename) {
@@ -96,12 +118,61 @@ TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_or_opt=yes pro
     kofola::OPTIONS.params.erase("tela_det_alg");
 }
 
-TEST_CASE("complement_tela with tela_det_alg=inductive and both sd_ind_sh_break=yes and sd_ind_or_opt=yes",
+TEST_CASE("complement_tela with tela_det_alg=inductive and sd_ind_or_opt=inf_tree produces language-equivalent results to Spot",
+          "[complement_tela][tela_det_alg][inductive][sd_ind_or_opt]") {
+    // Set up TELA options and enable inductive SD-TELA determinization with OR-FIN optimization
+    test_utils::setup_tela_options();
+    kofola::OPTIONS.params["tela_det_alg"] = "inductive";
+    kofola::OPTIONS.params["sd_ind_or_opt"] = "inf_tree";
+
+    for (const std::string& filename : test_utils::COMMON_TEST_FILES) {
+        SECTION("Testing file (inductive det, OR-FIN opt): " + filename) {
+            spot::twa_graph_ptr aut = test_utils::load_automaton_from_file(filename);
+            REQUIRE(aut != nullptr);
+
+            bool equivalent = test_utils::test_complement_equivalence(aut, false);
+            CHECK(equivalent);
+        }
+    }
+
+    // Clean up to avoid side-effects on other tests
+    kofola::OPTIONS.params.erase("sd_ind_or_opt");
+    kofola::OPTIONS.params.erase("tela_det_alg");
+}
+
+TEST_CASE("complement_tela with tela_det_alg=inductive and both sd_ind_sh_break=root and sd_ind_or_opt=yes",
           "[complement_tela][tela_det_alg][inductive][sd_ind_sh_break][sd_ind_or_opt]") {
     // Set up TELA options with both shared breakpoint and OR-FIN optimizations
     test_utils::setup_tela_options();
     kofola::OPTIONS.params["tela_det_alg"] = "inductive";
-    kofola::OPTIONS.params["sd_ind_sh_break"] = "yes";
+    kofola::OPTIONS.params["sd_ind_sh_break"] = "root";
+    kofola::OPTIONS.params["sd_ind_or_opt"] = "yes";
+
+    for (const std::string& filename : test_utils::COMMON_TEST_FILES) {
+        if(filename == "tests/test_data/random_sd_streett_006.hoa") {
+            continue; // skip this file which is a known outlier for the OR-FIN optimization
+        }
+        SECTION("Testing file (inductive det, shared breakpoint + OR-FIN opt): " + filename) {
+            spot::twa_graph_ptr aut = test_utils::load_automaton_from_file(filename);
+            REQUIRE(aut != nullptr);
+
+            bool equivalent = test_utils::test_complement_equivalence(aut, false);
+            CHECK(equivalent);
+        }
+    }
+
+    // Clean up to avoid side-effects on other tests
+    kofola::OPTIONS.params.erase("sd_ind_or_opt");
+    kofola::OPTIONS.params.erase("sd_ind_sh_break");
+    kofola::OPTIONS.params.erase("tela_det_alg");
+}
+
+TEST_CASE("complement_tela with tela_det_alg=inductive and both sd_ind_sh_break=inf_tree and sd_ind_or_opt=yes",
+          "[complement_tela][tela_det_alg][inductive][sd_ind_sh_break][sd_ind_or_opt]") {
+    // Set up TELA options with both shared breakpoint and OR-FIN optimizations
+    test_utils::setup_tela_options();
+    kofola::OPTIONS.params["tela_det_alg"] = "inductive";
+    kofola::OPTIONS.params["sd_ind_sh_break"] = "inf_tree";
     kofola::OPTIONS.params["sd_ind_or_opt"] = "yes";
 
     for (const std::string& filename : test_utils::COMMON_TEST_FILES) {
